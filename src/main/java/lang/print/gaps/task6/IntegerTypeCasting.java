ee86ae97e1b513e17e7745273d49f94b8a0b8af6
dzmitry <zakharenka.dzmitry.1981@gmail.com>
2022-08-15 14:47:38 +0300

    stage0-module3-task6

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+
+        long first = 1000;
+        int second = (int) first;
+
+        System.out.println(first);
+        System.out.println(second);
     }
 }
+
+//Create long variable with "first" name first and "1000" value. Assign to the variable another var,
+// int, with "second" name. Print both.
